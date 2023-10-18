ea15d17c9726f9e1e7a53a7d0e64b9bcf8960dee
Tommy Verdone <tommy@Tommys-MacBook-Pro.local>
2023-10-18 09:56:20 -0700

    init

---
+++ script.js
@@ -0,0 +1,26 @@
+const mammoth = require('mammoth');
+const fs = require('fs');
+
+const inputFile = 'template.doc';
+const outputFile = 'output.doc';
+
+const companyName = 'Your Company Name';
+const positionName = 'Software Developer';
+
+mammoth.extractRawText({ path: inputFile })
+  .then(function (result) {
+    let text = result.value;
+    text = text.replace(/COMPANY_NAME/g, companyName);
+    text = text.replace(/POSITION_NAME/g, positionName);
+
+    fs.writeFile(outputFile, text, function (err) {
+      if (err) {
+        console.error('Error writing to the output file:', err);
+      } else {
+        console.log('Text replaced and saved to', outputFile);
+      }
+    });
+  })
+  .catch(function (err) {
+    console.error('Error converting .doc:', err);
+  });
